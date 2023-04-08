e791fdec063b501dc1585debfeee85a1954b962a
Raghav-45 <77260113+Raghav-45@users.noreply.github.com>
2023-04-08 17:56:30 +0530

    Now Started using Chakra UI, Redesigned Everything

--- src/pages/LoginHelperPage.jsx
+++ src/pages/LoginHelperPage.jsx
@@ -1,35 +1,41 @@
 import React, { useState } from 'react'
 import { supabase } from '../lib/supabaseClient'
+import { Box, Button, Center, Heading, Input, Text } from '@chakra-ui/react'
 
 export default function LoginHelperPage() {
-  const [key, setKey] = useState()
-  const [email, setEmail] = useState()
-  const [password, setPassword] = useState()
+  const [key, setKey] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
 
   const handleClick = (e) => {
     e.preventDefault()
+    setIsLoading(true)
     const makeDriverLogin = async () => {
       const { data, error } = await supabase.from('keyboardless_login').insert({ key: key, email: email, password: password })
+      setTimeout(() => setIsLoading(false), 150)
     }
     makeDriverLogin()
   }
 
   return (
-    <div className="loginHelperContainer" style={{width: '100%'}}>
-      <h1>Login Helper for Drivers</h1>
-      <div>
-        <label for="key">Access Key</label>
-        <input value={key} onChange={(e) => setKey(e.target.value)} type="text" id="key" name="key" placeholder="Enter your access Key" />
+    <Center height={'full'} background={'#e1e2e1'}>
+      <Box background={'white'} color={'#555'} className="loginHelperContainer" p={'25px'} width={'full'} style={{boxShadow: '0px 0px 25px rgba(0, 0, 0, 0.2)'}}>
+        <Heading as='h2' size='lg' color={'#555'} mb={'20px'} textAlign={'center'}>Login Helper for Drivers</Heading>
+        <Box>
+          <Text fontWeight={500} mb='8px'>Access Key</Text>
+          <Input mb={'16px'} value={key} onChange={(e) => setKey(e.target.value)} type="text" id="key" name="key" placeholder="Enter your access Key" />
 
-        <label for="email">Email</label>
-        <input value={email} onChange={(e) => setEmail(e.target.value)} type="text" id="email" name="email" placeholder="Enter your email" />
+          <Text fontWeight={500} mb='8px'>Email</Text>
+          <Input mb={'16px'} value={email} onChange={(e) => setEmail(e.target.value)} type="text" id="email" name="email" placeholder="Enter your email" />
 
-        <label for="password">Password</label>
-        <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" id="password" name="password" placeholder="Enter your password" />
+          <Text fontWeight={500} mb='8px'>Password</Text>
+          <Input mb={'16px'} value={password} onChange={(e) => setPassword(e.target.value)} type="password" id="password" name="password" placeholder="Enter your password" />
 
-        {/* <input onClick={handleClick} type="submit" value={!isLoading ? "Login" : 'Loading'} style={{width: '100%'}} /> */}
-        <button onClick={handleClick} style={{width: '100%'}}>Login</button>
-      </div>
-    </div>
+          {/* <input onClick={handleClick} type="submit" value={!isLoading ? "Login" : 'Loading'} style={{width: '100%'}} /> */}
+          <Button colorScheme={'blue'} background={'blue.400'} onClick={handleClick} width={'full'} isLoading={isLoading}>Login</Button>
+        </Box>
+      </Box>
+    </Center>
   )
 }
